b7d4b33f1a0faa4acc24ca6d93dc4ec9a4cbb133
Marco Lima <marckdx@outlook.com>
2021-07-27 20:18:31 -0300

    documenting
    documenting IniUtils

--- Brokenegg.DotIni/Utils/IniUtils.cs
+++ Brokenegg.DotIni/Utils/IniUtils.cs
@@ -9,6 +9,11 @@ namespace Brokenegg.DotIni.Utils
 {
     public class IniUtils
     {
+        /// <summary>
+        /// Convert a string to List based on the new line character \n
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static List<string> StringToList(string text)
         {
             var context = new List<string>();
@@ -24,16 +29,36 @@ namespace Brokenegg.DotIni.Utils
             return typeof(string);
         }
 
+        /// <summary>
+        /// Check if the line is a key-par value
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static bool IsKeyPar(string line) => RegexExpressions.IsKeyPar(line);
-
+        
+        /// <summary>
+        /// Convert a ini key-par value to IniKey
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static IniKey StripeKeyPar(string line)
         {
             var content = RegexExpressions.StripeAnything(line, RegexExpressions.KeyParRegex, new List<int>() { 1, 3 });
             return content.Any() ? new IniKey(content.FirstOrDefault(), new IniValue(content[1])) : null;
         }
 
+        /// <summary>
+        /// Check if the line is a section
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static bool IsSection(string line) => RegexExpressions.IsSection(line);
 
+        /// <summary>
+        /// Convert a ini section to IniSection
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static IniSection StripeSection(string line)
         {
             return new IniSection(RegexExpressions.StripeAnything(line, RegexExpressions.SectionRegex, new List<int>() { 2 }).FirstOrDefault());
